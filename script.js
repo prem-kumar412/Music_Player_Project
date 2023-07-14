bf234ec789ce0fcc7218b4a4924bddbda0fb465d
onkar srivastava <imonkar29@gmail.com>
2023-07-15 01:27:55 +0530

    added clicking functionality on heart icon to select and deselect fav songs

---
+++ script.js
@@ -0,0 +1,308 @@
+// adding clicking functionality to the heart icon where you can select and deselect your favourite songs
+function toggleIcon1() {
+    var iconElement = document.getElementById("icon1");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon2() {
+    var iconElement = document.getElementById("icon2");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon3() {
+    var iconElement = document.getElementById("icon3");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon4() {
+    var iconElement = document.getElementById("icon4");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon5() {
+    var iconElement = document.getElementById("icon5");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon6() {
+    var iconElement = document.getElementById("icon6");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon7() {
+    var iconElement = document.getElementById("icon7");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon8() {
+    var iconElement = document.getElementById("icon8");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon9() {
+    var iconElement = document.getElementById("icon9");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon10() {
+    var iconElement = document.getElementById("icon10");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon11() {
+    var iconElement = document.getElementById("icon11");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon12() {
+    var iconElement = document.getElementById("icon12");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon13() {
+    var iconElement = document.getElementById("icon13");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon14() {
+    var iconElement = document.getElementById("icon14");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon15() {
+    var iconElement = document.getElementById("icon15");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon16() {
+    var iconElement = document.getElementById("icon16");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon17() {
+    var iconElement = document.getElementById("icon17");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon18() {
+    var iconElement = document.getElementById("icon18");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon19() {
+    var iconElement = document.getElementById("icon19");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon20() {
+    var iconElement = document.getElementById("icon20");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon21() {
+    var iconElement = document.getElementById("icon21");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
+
+function toggleIcon22() {
+    var iconElement = document.getElementById("icon22");
+
+    if (iconElement.classList.contains("fas")) {
+        // If the current icon is regular (far), change it to solid (fas)
+        iconElement.classList.remove("fas");
+        iconElement.classList.add("far");
+    } else {
+        // If the current icon is solid (fas), change it to regular (far)
+        iconElement.classList.remove("far");
+        iconElement.classList.add("fas");
+    }
+}
